3acade493225a4d9ddd48789a2a10cd56a12cc1e
igor <igor.nigay@gmail.com>
2016-12-19 16:42:08 -0800

    Adds Material Design Default Theme

---
+++ src/shared/Navigation.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+import AppBar from 'material-ui/AppBar';
+
+class Navigation extends Component
+{
+
+
+    render()
+    {
+        return (
+            <AppBar title="title">
+            </AppBar>
+        );
+        
+    }
+}
+
+export default Navigation;
