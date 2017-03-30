408c7f5003f81e5559995830fddb1fd04741a341
inigay <igor.nigay@gmail.com>
2017-03-29 23:55:03 -0700

    Adds Desktop Navigation

--- src/shared/Navigation.js
+++ src/shared/Navigation.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
-
+import {List, ListItem} from 'material-ui/List';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import NavigationArrowDropDown from 'material-ui/svg-icons/navigation/arrow-drop-down';
 import AppBar from 'material-ui/AppBar';
 
 class Navigation extends Component
@@ -10,7 +13,49 @@ class Navigation extends Component
     render()
     {
         return (
-            <AppBar style={{position:'fixed'}} className="app-bar" zDepth={0} title="Intigo" iconElementLeft={<Link to={'/'}><img src="/img/logo2.png" className="logo-image" /></Link>}>
+            <AppBar 
+                style={{position:'fixed'}} 
+                className="app-bar" 
+                zDepth={1} title="Intigo" 
+                iconElementLeft={
+                    <Link to={'/'}>
+                        <img 
+                            src="/img/logo2.png" 
+                            className="logo-image" 
+                        />
+                    </Link>
+                }
+                iconStyleRight={
+                    {flexGrow: 1}
+                }
+                iconElementRight={
+                    <Link to={'/'} style={{flexGrow: 1}}>
+                        <List 
+                            className="inline-nav"
+                            style={{display:"flex", justifyContent: "flex-end"}}
+                        >
+                            <ListItem 
+                                primaryText="Services" 
+                                className="nav-sub-menu"
+                                rightIcon={<NavigationArrowDropDown />}
+                            >
+                                <Menu
+                                    className="sub-menu-item"
+                                    
+                                >
+                                    <MenuItem primaryText="Web Development" />
+                                    <MenuItem primaryText="Mobile App Development" />
+                                    <MenuItem primaryText="Reporting" />
+                                    <MenuItem primaryText="SEO Services" />
+                                </Menu>
+                            </ListItem>
+                            <ListItem primaryText="About" />
+                            <ListItem primaryText="Case Study" />
+                            <ListItem primaryText="Contact" />
+                        </List>
+                    </Link>
+                }
+                >
             </AppBar>
         );
         
